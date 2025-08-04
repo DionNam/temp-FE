bdf35c599192054ec015e5049d9878f7aed4fda0
satriadhikara <84092642+satriadhikara@users.noreply.github.com>
2025-08-04 18:34:12 +0700

    fix : update onboarding page to use search parameters for URL

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,12 +1,18 @@
+"use client";
+
 import { OnboardLayout } from "@/components/onboarding/OnboardLayout";
 import OnboardingForm from "@/components/forms/OnboardingForm";
+import { useSearchParams } from "next/navigation";
 
 const OnboardingPage = () => {
+    const searchParams = useSearchParams();
+    const url = searchParams.get("url");
+
     return (
         <OnboardLayout>
-            <OnboardingForm />
+            <OnboardingForm url={url || undefined} />
         </OnboardLayout>
-    )
-}
+    );
+};
 
 export default OnboardingPage;
