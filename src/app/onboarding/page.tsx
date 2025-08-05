94e9f66a14b9592bd6e4cace186df0e0d5618692
satriadhikara <84092642+satriadhikara@users.noreply.github.com>
2025-08-05 11:55:28 +0700

    fix : remove unused search parameters from onboarding page

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,16 +1,10 @@
-"use client";
-
 import { OnboardLayout } from "@/components/onboarding/OnboardLayout";
 import OnboardingForm from "@/components/forms/OnboardingForm";
-import { useSearchParams } from "next/navigation";
 
 const OnboardingPage = () => {
-    const searchParams = useSearchParams();
-    const url = searchParams.get("url");
-
     return (
         <OnboardLayout>
-            <OnboardingForm url={url || undefined} />
+            <OnboardingForm />
         </OnboardLayout>
     );
 };
