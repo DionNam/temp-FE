0b08b1600a88cb54e4363375bffc872c12e2a46b
satriadhikara <84092642+satriadhikara@users.noreply.github.com>
2025-08-11 14:41:49 +0700

    feat(report): add Footer component to ReportPage and enhance Footer with className prop

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components/layout/Header";
 import ReportContent from "@/components/report/ReportContent";
+import { Footer } from "@/components/layout/Footer";
 
 const ReportPage = () => {
   return (
     <div className="bg-[#F0F5FE] flex flex-col items-center justify-center">
       <Header />
       <ReportContent />
+      <Footer className="w-full" />
     </div>
   );
 }
