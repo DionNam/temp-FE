5bd0b3bb07a0f1e54d432abf4bc1755622f5bed8
daehywon <ndh8392@gmail.com>
2025-09-11 18:38:01 +0900

    end

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -10,7 +10,7 @@ interface FooterProps {
 const Footer = React.memo(function Footer({ className }: FooterProps) {
   return (
     <footer className={`relative bg-gradient-to-b from-[#66afff00] to-[#0669ff00] py-16 sm:py-20 md:py-24 lg:py-32 px-4 sm:px-6 lg:px-8 overflow-hidden min-h-[400px] sm:min-h-[450px] md:min-h-[500px] lg:min-h-[600px] ${className}`}>
-      <div className="absolute inset-0 flex items-center justify-center">
+      <div className="absolute inset-0 flex items-end justify-center">
         <div className="w-full max-w-[2000px] relative px-4">
           <Image
             src="/ShowOnAI_text.svg"
